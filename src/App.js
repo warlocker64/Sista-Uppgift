1a6147887fe9e286d871d79a059f4c098499a699
jack <jack@gmail.cc>
2022-04-19 12:10:03 +0200

    Balls'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Nav from './Navigation/Nav';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> ....
         </p>
+        <Nav></Nav>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Navigation/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.css'
+function Nav (){
+    return(
+        <div>
+            
+            <h1 className="text1">Im my </h1>
+        </div>
+    )
+}
+
+export default Nav;
